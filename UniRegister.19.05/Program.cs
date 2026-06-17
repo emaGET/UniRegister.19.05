6119d675df498cb94bf389600ed2ffc480c75b0a
emaGET <emanuelepini@getacademy.no>
2026-06-17 11:56:00 +0200

    forgot var app = new App(); app.Start; ... sigh

--- UniRegister.19.05/Program.cs
+++ UniRegister.19.05/Program.cs
@@ -2,17 +2,20 @@
 {
     internal class Program
     {
-        // static void Main(string[] args)
-        // {
+        static void Main(string[] args)
+        {
+
+            var app = new App();
+            app.Start;
             // List<Student> students = new List<Student>(); this is now in app
-            
+
             // var student1 = 
             // students.Add(new Student("Jorge Jorgensen", 27 , "jj2726", "Law and Order", 1, 943162, 60));
             // var student2 = 
             // students.Add(new Student("Bjørn Bjørnsen", 28 , "bb2826", "CompSci", 2, 243160, 90));
             // var student3 = 
             // students.Add(new Student("Morten Mortensen", 29, "mm2926", "History", 1, 073195, 30));
-            
+
             // student1.PrintInfo();
             // student2.PrintInfo();
             // student3.PrintInfo();
@@ -20,7 +23,7 @@
             // students[0].PrintInfo();
             // students[1].PrintInfo();
             // students[2].PrintInfo();
-            
+
             // and now we further improve it by using a foreach!
 
             // foreach (Student student in students)
@@ -35,6 +38,6 @@
 
             // students[1].StudentCard();
 
-        // }
+        }
     }
 }
